eb5534fac919505248ad6d2f590300e58206626c
AfanasevEvgeny <afazhenya2001@gmail.com>
2022-02-04 14:18:01 +0300

    Initialized project, add tailwind

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <MainLayout/>
+  </div>
+</template>
+
+<script>
+
+import MainLayout from "@/components/MainLayout";
+
+export default {
+  name: 'App',
+  components: {
+    MainLayout
+  }
+}
+</script>
+
+<style>
+
+</style>
